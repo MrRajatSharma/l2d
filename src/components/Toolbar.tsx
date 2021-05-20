7036b07c11c088891ef8b9ecfbdc3f626a8a2011
MrRajatSharma <er.rajat15@gmail.com>
2021-05-20 22:59:44 +0530

    toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faGooglePlus } from '@fortawesome/free-brands-svg-icons';
 
 function Toolbar() {
   return (
       <div id="topbar" className="d-none d-lg-block">
-        <div className="container clearfix">
+        <div className="clearfix">
           <div className="contact-info float-left">
-            <FontAwesomeIcon icon={faEnvelope} size="xs" className="mr-2"/>
-            <a href="mailto:contact@example.com" className="mr-2">contact@example.com</a>
+            <FontAwesomeIcon icon={faEnvelope} className="mr-2"/>
+            <a href="mailto:info.learn2motor@gmail.com" className="mr-2">info.learn2motor@gmail.com</a>
             <FontAwesomeIcon icon={faPhone} className="mr-2" />
-            +1 5589 55488 55
+            +91-6005488940, +91-9055000026
           </div>
           <div className="social-links float-right">
-            <a href="#" className="twitter"><i className="fa fa-twitter"></i></a>
-            <a href="#" className="facebook"><i className="fa fa-facebook"></i></a>
-            <a href="#" className="instagram"><i className="fa fa-instagram"></i></a>
-            <a href="#" className="google-plus"><i className="fa fa-google-plus"></i></a>
-            <a href="#" className="linkedin"><i className="fa fa-linkedin"></i></a>
+            <a href="#" className="faFacebook">
+              <FontAwesomeIcon icon={faFacebook} className="mr-2"/>
+            </a>
+            <a href="#" className="faTwitter">
+              <FontAwesomeIcon icon={faTwitter} className="mr-2"/>
+            </a>
+            <a href="#" className="faGooglePlus">
+              <FontAwesomeIcon icon={faGooglePlus} className="mr-2"/>
+            </a>
           </div>
         </div>
       </div>
