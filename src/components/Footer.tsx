59c33ad1475cd508c098fc371403e78974487841
MrRajatSharma <er.rajat15@gmail.com>
2021-05-20 23:59:36 +0530

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default () => (
+  <>
+    <footer id="footer">
+      <div className="container">
+        <div className="copyright">
+          &copy; Copyright <strong>L2M</strong>. All Rights Reserved
+        </div>
+        <div className="credits">
+          Drive with Confidence
+        </div>
+      </div>
+    </footer>
+
+    <a href="#" className="back-to-top"><i className="fa fa-chevron-up"></i></a>
+  </>
+)
