37583b72d5564f420fcd465a77bb30e90ac4f32d
MrRajatSharma <er.rajat15@gmail.com>
2021-05-22 23:25:26 +0530

    phone button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,25 @@
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 export default () => (
   <header id="header">
-  <div className="container">
+  <div className="container d-flex align-items-center justify-content-between">
 
     <div id="logo" className="float-left">
       {/* <h1><a href="/" className="scrollto">Learn<span>2</span>Motor</a></h1> */}
 
-      <a href="#body"><img src="logo2.jpg" alt="" title="" /></a>
+      <a href="#body"><img src="logo2.jpg" alt="Drive 2 Motor " title="Drive 2 Motor" /></a>
     </div>
 
-    <nav id="nav-menu-container">
+    <nav id="nav-menu-container" className="d-flex align-items-center ">
       <ul className="nav-menu">
-        <li className="menu-active"><a href="#body">Home</a></li>
+        <li className="menu-active"><a href="/">Home</a></li>
         <li><a href="#about">About Us</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li className="menu-has-children"><a href="">Drop Down</a>
-          <ul>
-            <li><a href="#">Drop Down 1</a></li>
-            <li><a href="#">Drop Down 3</a></li>
-            <li><a href="#">Drop Down 4</a></li>
-            <li><a href="#">Drop Down 5</a></li>
-          </ul>
-        </li>
         <li><a href="#contact">Contact</a></li>
       </ul>
+      <a href="tel:+916005488940" className="btn btn-outline-primary ml-3"><FontAwesomeIcon icon={faPhone} /> Call us now</a>
     </nav>
   </div>
 </header>
