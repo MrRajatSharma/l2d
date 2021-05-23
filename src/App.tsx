94c4eeaadd8959b2e0a617e725905f22d0beae1f
MrRajatSharma <er.rajat15@gmail.com>
2021-05-23 08:26:44 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -7,18 +7,11 @@ import Toolbar from "./components/Toolbar";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-function App() {
-    const [modal, setModal] = useState(false);
-    const toggle = () => setModal(!modal);
-
-    return (
-        <div className="App py-3">
-            <Toolbar />
-            <Header />
-            <Main />
-            <Footer />
-        </div>
-    );
-}
-
-export default App;
+export default () => (
+  <div className="App">
+    <Toolbar />
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
